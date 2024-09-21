e32c7ed47e1552fa2d1931d8c03441739bf3b9f0
Sebastian Romo <sromo1@umd.edu>
2024-09-21 13:15:38 -0400

    5. Make the app prettier

---
+++ namer_app/lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:math';
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext(){
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random ORIANE idea:'),
+            BigCard(pair: pair),
+            ElevatedButton(
+              onPressed: () {
+                print('button pressed');
+                appState.getNext();
+              },
+              child: Text('Next'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      fontStyle: FontStyle.italic,
+    );
+    return SizedBox(
+      width: 400,
+      height: 150,
+      child: Card(
+        color: theme.colorScheme.primary,
+        elevation: 50,
+         child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Center(
+            child: Text(
+              pair.asLowerCase, 
+              style:style,
+              semanticsLabel: "${pair.first} ${pair.second}",
+            ),
+          )
+         )
+      )
+    );
+  }
+}
